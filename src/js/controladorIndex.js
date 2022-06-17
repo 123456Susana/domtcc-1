06f2664f4820f7afd265551ba4e53de322fad5ab
Your Name <you@example.com>
2022-06-17 12:11:26 -0500

    se hacen arrays

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,48 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
 
-if(true){
-    var nombre="Lucho diaz"
+//CONSTRUYENDO ARREGLOS
+
+//1.FORMA TRADICIONAL
+let numeros=[1,2,3,4,5]
+let nombres=['juan', 'camila', 'andres', 'catalina']
+let votos=[true,true,false,false,false, true]
+
+
+
+//2.FORMA POO
+let numerosPOO=Array(1,2,3,4,5)
+let nombresPOO=Array('juan', 'camila', 'andres', 'catalina')
+let votosPOO=Array(true,true,false,false,false, true)
+
+//IMPRIMIR LOS ELEMENTOS DEL ARREGLO
+console.log(numeros)
+console.log(numerosPOO)
+
+//ACCEDER A UN SOLO ELEMENTO DEL ARREGLO
+console.log(numeros[3])
+console.log(numeros[4]) //aparece en la consola el numero 5 porque esta en la posicion 5(empieza a contar en 0)
+
+//OBJETOS EN JS
+let persona={
+    nombre:'juan',
+    edad:24,
+    hinchaDelnacional:false,
+    foto: 'url',
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+
+console.log(persona)
+console.log(persona.nombre)
+
+
+
+
+//en vez de hacer esto se hace array:
+/*let nombreProducto = 'Funko pop de Petro'
+let precioProducto=45000
+let cantidadProducto=5
+let imagenProducto='NA'
+
+let identificacionUsuario='1036676219'
+const IVA=0.19
+const PAIS_USUARIO='COL'
+
+let totalCompra=precioProducto*cantidadProducto*/
---
+++ src/js/ejercicioproductos.js
@@ -0,0 +1,210 @@
+let producto1={
+    nombre:'Pelicula',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto2={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto3={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto4={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto5={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto6={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto7={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto8={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto9={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto10={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto11={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto12={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto13={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto14={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto15={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto16={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto17={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto18={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto19={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto20={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto21={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto22={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto23={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto24={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto25={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto26={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto27={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto28={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto29={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
+let producto30={
+    nombre:'Cama',
+    precio: 1200000,
+    cantidad: 1,
+    foto:'',
+    descripcion:'Cama madera 2*2'
+}
